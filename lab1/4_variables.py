ef29b8c261fdfd92cf22137a44023f159f20f90d
Yerkebulan Yessimbek <erkebulanesimbek@gmail.com>
2025-01-18 14:33:11 +0500

    update

--- lab1/4_variables.py
+++ lab1/4_variables.py
@@ -3,6 +3,6 @@ y = 'John'
 print(type(x))
 print(type(y))
 
-a = str(3)    # x will be '3'
-b = int(3)    # y will be 3
-c = float(3)  # z will be 3.0
+a = str(3)    # a will be '3'
+b = int(3)    # b will be 3
+c = float(3)  # c will be 3.0
