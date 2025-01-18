408d56d7e0cf2a8bdb3b5904bfa791f62c4f848c
Yerkebulan Yessimbek <erkebulanesimbek@gmail.com>
2025-01-18 14:42:27 +0500

    update

--- lab1/4_variables.py
+++ lab1/4_variables.py
@@ -6,3 +6,18 @@ print(type(y))
 a = str(3)    # a will be '3'
 b = int(3)    # b will be 3
 c = float(3)  # c will be 3.0
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+"""
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+"""
